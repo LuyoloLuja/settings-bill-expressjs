bff278d1b6c8c0fe230c765af0cd83be35c932b7
Luyolo <luyolom19@gmail.com>
2020-08-27 16:46:02 +0200

    rounded off my  totals to two decimals

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
+let moment = require('moment');
 
 const SettingsBill = require('./settings-bill');
 const settingsBill = SettingsBill();
